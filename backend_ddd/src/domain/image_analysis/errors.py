9934d162b94c50a3a97defe7a2652777cfacc4ca
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2025-11-10 08:22:39 -0300

    feat(backend/domain): enhance error handling
    - Add specific domain exceptions
    - Implement validation errors for entities
    - Add invariant violation checks
    - Improve error messages and context

--- backend_ddd/src/domain/image_analysis/errors.py
+++ backend_ddd/src/domain/image_analysis/errors.py
@@ -1,31 +1,53 @@
 from src.domain.shared.errors import DomainException, InvalidValue
 
 
-class ClothNotFoundException(DomainException):
+class InvalidTransitionException(Exception):
     """
-    Raised when not finding any cloth pieces in a input image.
+    Raised when a transition call comes from a unexpected state
     """
-    pass
+
+
+class MissingItemException(Exception):
+    """
+    Raised when a item is missing
+    """
+
+
+class InvariantViolationException(Exception):
+    """
+    Raised when a invariant is broken
+    """
+
+
+class InvalidLabel(InvalidValue):
+    """
+    Raised when a label is empty
+    """
+
 
 class InvalidImageDimension(InvalidValue):
     """
     Raised when an image's width or height exceeds the maximum or the minimum allowed pixel dimensions.
     """
 
+
 class InvalidImageType(InvalidValue):
     """
     Raised when the image type is not supported.
     """
 
+
 class InvalidImageBlobSize(InvalidValue):
     """
     Raised when the image blob size exceeds the max size or is empty.
     """
-    
+
+
 class InvalidBoundingBox(InvalidValue):
     """
     Raised when the bounding box of a cloth item is invalid.
     """
+
 
 class InvalidImageAnalysisStatusType(InvalidValue):
     """
